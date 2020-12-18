e1a54ba04beb437cea0f082ed618752953757374
Marco Schumacher <marco@schumacher.dev>
2020-12-18 23:41:07 +0100

    Added BABEL_INCLUDE

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -400,7 +400,7 @@ module.exports = function (webpackEnv) {
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
-              include: paths.appSrc,
+              include: [paths.appSrc, process.env.BABEL_INCLUDE],
               loader: require.resolve('babel-loader'),
               options: {
                 customize: require.resolve(
